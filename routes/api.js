dff8520c1f583f691936a57da7a4404cfbf44bfc
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-07-27 20:33:09 +0300

    fix post

--- routes/api.js
+++ routes/api.js
@@ -13,17 +13,38 @@ module.exports = function (app) {
           .status(StatusCodes.NOT_FOUND)
           .send("No project called " + project + " found");
       }
+      issues = issues.map((d) => ({
+        _id: d._id,
+        issue_title: d.issue_title,
+        issue_text: d.issue_text,
+        created_on: d.createdAt,
+        updated_on: d.updatedAt,
+        created_by: d.created_by,
+        assigned_to: d.assigned_to,
+        open: d.open,
+        status_text: d.status_text
+      }));
       return res.status(StatusCodes.OK).json(issues);
     })
 
     .post(async function (req, res) {
       let project = req.params.project;
-
       let { body } = req;
       if (!body["issue_title"] || !body["issue_text"] || !body["created_by"])
         return res.send({ error: "required field(s) missing" });
       body = { ...body, project };
-      const issue = await Issue.create(body);
+      let issue = await Issue.create(body);
+      issue = {
+        _id: issue._id,
+        issue_title: issue.issue_title,
+        issue_text: issue.issue_text,
+        created_on: issue.createdAt,
+        updated_on: issue.updatedAt,
+        created_by: issue.created_by,
+        assigned_to: issue.assigned_to,
+        open: issue.open,
+        status_text: issue.status_text
+      };
       return res.status(StatusCodes.CREATED).json(issue);
     })
 
